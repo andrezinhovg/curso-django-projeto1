1e4fc506a55a125e0342250b8599e9e9acb72657
andre <andrezinhoguimaraesoficial@gmail.com>
2025-04-27 09:16:55 -0400

    Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,13 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 
 def home(request):
      return render(request, 'recipes/home.html', context={
          'name': 'André Guimarães',
      })
 
-def contato(request):
-     return render(request, 'recipes/contato.html')
-
-def sobre(request):
-     return HttpResponse('SOBRE')
